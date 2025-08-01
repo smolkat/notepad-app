dbb9ee25a65ab98693cbfeece365c8677231464b
Aditya Banerjee <a.banerjee6762@gmail.com>
2025-08-01 23:02:27 +0530

    Update NotrApplication.java
    test env variables

--- src/main/java/com/note/random/notr/NotrApplication.java
+++ src/main/java/com/note/random/notr/NotrApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NotrApplication {
 
 	public static void main(String[] args) {
+		System.out.println("MYSQL_URL = " + System.getenv("MYSQL_URL"));
 		SpringApplication.run(NotrApplication.class, args);
 	}
 
